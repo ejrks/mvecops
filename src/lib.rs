33981ebddfc1b32eec0a4558477abf8486514d52
EternityLabs <eternitylabs@protonmail.com>
2024-12-22 13:12:57 +0100

    Accumulation steps complete

--- src/lib.rs
+++ src/lib.rs
@@ -26,17 +26,17 @@ fn textfile_to_int_vector(file_path: String) -> Result<Vec<u32>, Box<dyn Error>>
 #[derive(Clone)]
 struct Vmatrix<T>
 where
-    T: Number,
+    T: Number + ToString + Clone,
 {
     size: usize,
     data: Vec<T>,
 }
 
 
-
+// Initialize and build should be the same method
 impl<T> Vmatrix<T> 
 where
-    T: Number,
+    T: Number + ToString + Clone,
 {
     fn build(size: usize, new_data: Vec<T>) -> Vmatrix<T> {
         let mut new_instance = Vmatrix {
@@ -51,6 +51,20 @@ where
         new_instance
     }
 
+    fn initialize(size: usize, initial_value: T) -> Vmatrix<T> {
+        let mut new_instance = Vmatrix {
+            data: Vec::new(),
+            size,
+        };
+
+        let total_data_size = size * size;
+        for x in 0..total_data_size {
+             new_instance.data.push(initial_value.clone());
+        }
+
+        new_instance
+    }
+
     fn new(size: usize) -> Vmatrix<T> {
         Vmatrix {
             data: Vec::new(),
@@ -58,8 +72,17 @@ where
         }
     }
 
-    fn write_to_file(&self) {
+    fn write_to_file(&self, file_path: String) {
+        let mut complete_output: String = String::from("");
+        let size = self.size;
 
+        for i in 0..size {
+            for j in 0..size {
+                complete_output += &self.data[j + i * size].to_string();
+            }    
+            complete_output += &String::from("\n");
+        }
+        fs::write(file_path, complete_output);
     }
 }
 
@@ -82,7 +105,8 @@ fn set_bound_rows_to_zero(input_data: &mut Vmatrix<u32>) {
     }
 }
 
-fn process_corners(input_data: &Vmatrix<u32>, output_data: &mut Vmatrix<u32>) -> bool{
+// Porperly factor out pointer counting
+fn process_corners(input_data: &Vmatrix<u32>, output_data: &mut Vmatrix<u32>) -> bool {
     let mut two_points_in_a_row = false;
     let mut previous_active = false;
 
@@ -95,24 +119,32 @@ fn process_corners(input_data: &Vmatrix<u32>, output_data: &mut Vmatrix<u32>) ->
     let mut dw_pointer = row_size * 2 + 1;
     let last_pointer = (row_size * row_size) - row_size;
 
-    let mut all_pointers_up = |mut x, mut y, mut z| {
-        x += 1;
-        y += 1;
-        z += 1;
-    };
-
     let mut surrounding_all_non_zero = false;
 
+    let mut written_times = 0;
+
     while current_pointer != last_pointer {
+        if written_times >= 500 && written_times <= 510 {
+                output_data.write_to_file(String::from("dataat5xx.txt"));
+        }
+
         pointer_module = current_pointer % row_size;
         if pointer_module == 0 {
             previous_active = false;
-            all_pointers_up(current_pointer, up_pointer, dw_pointer);
+            
+            current_pointer += 1;
+            up_pointer += 1;
+            dw_pointer += 1;
+
             continue;
         }
         if pointer_module == (row_size - 1) {
-            previous_active == false;
-            all_pointers_up(current_pointer, up_pointer, dw_pointer);
+            previous_active = false;
+
+            current_pointer += 1;
+            up_pointer += 1;
+            dw_pointer += 1;
+
             continue;
         }
 
@@ -141,7 +173,9 @@ fn process_corners(input_data: &Vmatrix<u32>, output_data: &mut Vmatrix<u32>) ->
             previous_active = false;
         }
 
-        all_pointers_up(current_pointer, up_pointer, dw_pointer);
+        current_pointer += 1;
+        up_pointer += 1;
+        dw_pointer += 1;
     }
 
     two_points_in_a_row
@@ -149,7 +183,7 @@ fn process_corners(input_data: &Vmatrix<u32>, output_data: &mut Vmatrix<u32>) ->
 
 fn decorner_once(input_data: &Vmatrix<u32>, two_points_in_row: &mut bool) -> Vmatrix<u32> {
     // This might be wrong, initialization should be on zero
-    let mut result: Vmatrix::<u32> = Vmatrix::<u32>::build(input_data.size, input_data.data.clone());
+    let mut result: Vmatrix::<u32> = Vmatrix::<u32>::initialize(input_data.size, 0);
 
     set_bound_rows_to_zero(&mut result);
     *two_points_in_row = process_corners(&input_data, &mut result);
@@ -157,32 +191,57 @@ fn decorner_once(input_data: &Vmatrix<u32>, two_points_in_row: &mut bool) -> Vma
     result
 }
 
-fn get_accumulation(input_data: Vmatrix<u32>, write_out: Option<bool>) -> Vmatrix<u32> {
-    let mut working_data = input_data.clone();
+// Data that is gonna be access should hold a reference instead of fetching it every iteration
+fn accumulate_reductions(input_data: &Vec::<Vmatrix<u32>>) -> Vmatrix<u32> {
+    let mut data_length: usize = input_data[0].data.len();
 
-    let writing_out: bool = write_out.unwrap_or(false);
+    let mut result: Vmatrix<u32> = Vmatrix::<u32>::initialize(input_data[0].size, 0);
+
+    for item in input_data {
+        for x in 0..data_length {
+            result.data[x] = result.data[x] + item.data[x];
+        }
+    }
+
+    result
+}
+
+// Refactor to choose output files
+fn get_accumulation(input_data: Vmatrix<u32>, write_out: bool) -> Vmatrix<u32>{
+    let mut working_data = input_data.clone();
 
     let mut reductions: u32 = 1;
     let mut process: bool = true;
-    let mut accumulativeData: Vec::<Vmatrix<u32>> = Vec::new();
+    let mut accumulative_data: Vec::<Vmatrix<u32>> = Vec::new();
 
     while process && reductions < MAXIMUM_REDUCTIONS_DECORNERING {
         let new_data: Vmatrix<u32> = decorner_once(&working_data, &mut process);
 
-        accumulativeData.push(new_data.clone());
+        accumulative_data.push(new_data.clone());
 
         working_data = new_data;
+
+        if write_out {
+            let path_name: String = 
+                SAMPLE_OUTPUT_RED.to_owned() + 
+                &reductions.to_string() + 
+                &String::from(".txt");
+
+            working_data.write_to_file(path_name);
+        }
 
         reductions += 1;
     };
 
-    Vmatrix::<u32>::new(0)
+    accumulate_reductions(&accumulative_data)
 }
 
 // --- //
 
 const SAMPLE_INPUT_PATH: &str = "samplekanji.txt";
 const SAMPLE_OUTPUT_CLEAR: &str = "sampleoutput.txt";
+const SAMPLE_OUTPUT_RED: &str = "reduction#";
+const SAMPLE_OUTPUT_ACC: &str = "accumulations.txt";
 
 #[cfg(test)]
 mod tests {
@@ -214,7 +273,7 @@ mod tests {
     }
 
     #[test]
-    fn bound_rows_zero_working() {
+    fn bound_rows_zero() {
         let sample_data_3 = vec![1, 0, 1, 1, 1, 0, 0, 0, 1];
         let sample_comp_3 = vec![0, 0, 0, 1, 1, 0, 0, 0, 0];
         let sample_data_4 = vec![1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1];
@@ -247,10 +306,20 @@ mod tests {
         let mut complete_message: String = String::from("");
         for i in 0..sample_size {
             for j in 0..sample_size {
-                complete_message += &String::from("1");
+                complete_message += &String::from("9");
             }    
             complete_message += &String::from("\n");
         }
         fs::write(SAMPLE_OUTPUT_CLEAR, complete_message);
+    }
+
+    #[test]
+    fn write_out_sample_reductions() {
+        let sample_size: usize = 64;
+
+        let input_data: Vmatrix<u32> = textfile_to_vmatrix(SAMPLE_INPUT_PATH.to_string(), sample_size);
+
+        let accumulations: Vmatrix<u32> = get_accumulation(input_data, false);
+        accumulations.write_to_file(SAMPLE_OUTPUT_ACC.to_string());
     }
 }
